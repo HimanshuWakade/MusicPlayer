589c63243da97b97100b0f5e6eaf1140ef769772
Himanshu Wakade <hwakade2@gmail.com>
2023-06-15 11:57:44 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let controls = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+function playPause() {
+  if (ctrlIcon.classList.contains("fa-pause")) {
+    song.pause();
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+  } else {
+    song.play();
+    ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
+  }
+}
+//moving slider with music and pasue at the music pasuses
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
+
+progress.onchange = function () {
+  song.play();
+  song.currentTime = progress.value;
+  ctrlIcon.classList.add("fa-pause");
+  ctrlIcon.classList.remove("fa-play");
+};
